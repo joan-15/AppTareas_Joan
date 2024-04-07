1e56159d0478abb877bf2ca20a7c2df0c34ed58d
joan-15 <159147082+joan-15@users.noreply.github.com>
2024-04-07 15:42:05 -0500

    animacion

---
+++ app/src/main/java/com/joan/apptareas/MainActivity.java
@@ -0,0 +1,42 @@
+package com.joan.apptareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // Corrección aquí
+        setContentView(R.layout.activity_main);
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView joantextView = findViewById(R.id.joantextView);
+        TextView dispositivostextView = findViewById(R.id.dispositivostextView);
+        ImageView logoImageView = findViewById(R.id.logo);
+
+        joantextView.setAnimation(animacion2);
+        dispositivostextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, TareasActivity.class); // Corrección aquí
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+    }
+}
